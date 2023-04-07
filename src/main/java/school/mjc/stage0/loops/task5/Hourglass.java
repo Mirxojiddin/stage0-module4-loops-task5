d36d019af9493c7245feb77d3132d67553e497ce
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-07 21:45:57 +0500

    javob 1

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i=1; i<height; i++){
+            for (int j=1; j<height; j++){
+                if (i==1|| i==height)
+                    System.out.print("8 ");
+                else 
+                    if ((i<=j &&  height-i>=j) || (i>=j &&  height-i<=j))
+                        System.out.print("8 ");    
+                    else
+                        System.out.print("  ");
+                        
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Hourglass hourglass = new Hourglass();
+        hourglass.printHourglassOfGivenSize(9);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        int middle = cathetusLength/2;
+        for (int i=1; i<cathetusLength; i++){
+            for (int j=1; j<cathetusLength; j++){
+                if ((j==middle && i==1)|| j+i-1==middle || j==middle+i-1 || i==middle)
+                    System.out.print("8 ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.printTriangle(8);
     }
 }
